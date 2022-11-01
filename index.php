64c509f91d4da3bb6f50d9e387fb6f40a7d0e40b
rvsfara <rvsfara@gmail.com>
2022-11-01 18:58:24 -0300

    comandos if, e switch case

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
     echo "Meu Código PHP!";
-    echo "Causar conflito editando direto no github";
-    echo "não sabia que estava editando";
-    echo "Causar novo conflito editando direto no github";
+    echo "<br/>";
+    echo "Rafael Vieira dos Santos";
+    echo "<br/>";
+    echo "rvsfara@gmail.com";
+    echo "<br/>";
+    echo "+55 (55) 9 9639 7802"."<br/>";
+    $path = ".";//caminho onde quer listar
+    $diretorio = dir($path);
+    echo "Lista de Arquivos do diretório '<strong>".$path."</strong>':<br />";
+    while($arquivo = $diretorio -> read()){
+        echo "<a href='".$path.$arquivo."'>".$arquivo."</a><br/>";
+    }
+    $diretorio -> close();
 ?>
