4c5602b32b0abfaadd88c93653467108487cc1dd
rvsfara <rvsfara@gmail.com>
2022-11-03 20:52:08 -0300

    comandos break e continue for

--- controle_repeticao/laco.php
+++ controle_repeticao/laco.php
@@ -1,11 +1,18 @@
 <?php
-    for ($i = 0; $i < 1000; $i+=5){
-        if($i >= 200 && $i <=800){
+    for ($i = 0; $i < 100; $i+=5){
+        if($i >= 20 && $i <=80){
             continue;
         }
-        if( i === 900){
+        if($i === 90){
             break;
         }
         echo $i . "<br/>";
+    }
+    echo "Final do Primeiro Laço" . "<br/>";
+    for($i = 0; $i<10; $i--){
+        echo $i . " " . "<br/>";
+        if($i === -50){
+            break;
+        }
     }
 ?>
