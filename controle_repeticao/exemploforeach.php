9a3799d422733e5b319d6413b8e9fdb81d65de68
rvsfara <rvsfara@gmail.com>
2022-11-10 18:07:07 -0300

    exemplo foreach

---
+++ controle_repeticao/exemploforeach.php
@@ -0,0 +1,24 @@
+<form>
+    <input type="text" name="nome">
+    <input type="date" name="nascimento">
+    <input type="submit" value="OK">
+</form>
+<?php
+    $meses = array(
+        "Janeiro", "Fevereiro", "Março",
+        "Abril", "Maio", "Junho",
+        "Julho", "Agosto", "Setembro",
+        "Outubro", "Novembro", "Dezembro"
+    );
+    foreach ($meses as $index => $mes) {
+        echo "Índice: ".$index."<br/>";
+        echo "O mês é ".$mes. "<br/><br/>";
+    }
+    if(isset($_GET)){
+        foreach ($_GET as $key => $value) {
+            echo "Nome do campo: " . $key . "<br/>";
+            echo "Valor do campo: " . $value . "<br/>";
+            echo "<hr/>";
+        }
+    }
+?>
